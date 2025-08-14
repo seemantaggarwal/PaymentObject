bf803dec327218babd44823163e50d7bd4f6f744
seemantaggarwal <seemantanishth@gmail.com>
2025-08-15 01:09:38 +0530

    Adding Wallet Payments

--- src/Client.java
+++ src/Client.java
@@ -4,9 +4,12 @@ public class Client {
         ps.addPaymentMethod("seemantDC", new DebitCard("seemant", "someName"));
         ps.addPaymentMethod("seemantCC", new CreditCard("seemant", "someName"));
         ps.addPaymentMethod("seemantUPI", new UPI("seemant"));
+        ps.addPaymentMethod("seemantWallet", new Wallet());
+
         ps.makePayment("seemantDC");
         ps.makePayment("seemantCC");
         ps.makePayment("seemantUPI");
+        ps.makePayment("seemantWallet");
 
     }
 }
