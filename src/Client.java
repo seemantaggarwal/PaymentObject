0fc7c6e194d6cfa4b3f23f833a80de50e96d4a8c
seemantaggarwal <seemantanishth@gmail.com>
2025-08-15 00:59:23 +0530

    Adding extract payment features

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,12 @@
 public class Client {
+    public static void main(String[] args) {
+        PaymentService ps = new PaymentService();
+        ps.addPaymentMethod("seemantDC", new DebitCard("seemant", "someName"));
+        ps.addPaymentMethod("seemantCC", new CreditCard("seemant", "someName"));
+        ps.addPaymentMethod("seemantUPI", new UPI("seemant"));
+        ps.makePayment("seemantDC");
+        ps.makePayment("seemantCC");
+        ps.makePayment("seemantUPI");
+
+    }
 }
--- src/UPI.java
+++ src/UPI.java
@@ -1,7 +1,10 @@
 public class UPI implements PaymentMethod {
     private String UPIid;
+    public UPI(String UPIid) {
+        this.UPIid = UPIid;
+    }
     @Override
     public void pay() {
-
+        System.out.println("UPI Pay");
     }
 }
